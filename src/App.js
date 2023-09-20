3a295a412e201eb63ea17cfd3cd2b777a48225bb
SadzakovicFilip <filipsadzakovic.dev@gmail.com>
2023-09-20 14:43:37 +0200

    Using filter() and include() to search trough the list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
 import { Component } from "react";
 
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
+    //always runs first
+
     super();
     this.state = {
       monsters: [],
+      searched: "",
     };
+    console.log(`constructor`);
   }
-  
 
-  componentDidMount(){
+  componentDidMount() {
+    //this runs third
+    console.log(`componentDidMount`);
     fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then(response => response.json())
-    .then((users)=>this.setState(()=>{
-      return {monsters:users}
-    },()=>{console.log(this.state)}))
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
+    //this runs second
+    console.log(`render`);
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => {
+            const searchedString = e.target.value.toLowerCase()
+            const filterdMonsters = this.state.monsters.filter((item) => {
+              return item.name.toLowerCase().includes(searchedString);
+            });
+            this.setState(()=>{
+              return{monsters:filterdMonsters}
+            })
+          }}
+        />
         {this.state.monsters.map((item) => {
           return <h1 key={item.id}>{item.name}</h1>;
         })}
