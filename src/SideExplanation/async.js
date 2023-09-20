50b12d23759d919042fe862732960d9de44e0748
SadzakovicFilip <filipsadzakovic.dev@gmail.com>
2023-09-20 12:36:46 +0200

    async await explained

--- src/SideExplanation/async.js
+++ src/SideExplanation/async.js
@@ -10,4 +10,21 @@ export const myPromise = new Promise((resolve, reject) => {
   }
 });
 
-myPromise.then((value) => console.log(value)).catch(rejectedValue=>console.log(rejectedValue));
+myPromise
+  .then((value) => console.log(value))
+  .catch((rejectedValue) => console.log(rejectedValue));
+
+// NOVI ASYNC AWAIT  u es7, laksi nacin za organizovanje
+
+const myAsyncFunction = async () => {
+  try {
+    const usersResponse = await fetch(
+      `https://jsonplaceholder.typicode.com/users`
+    );
+    const users = await usersResponse.json();
+    const secondUser = users[1];
+    console.log(secondUser);
+  } catch (err) {
+    console.log("error has occured")
+  }
+};
