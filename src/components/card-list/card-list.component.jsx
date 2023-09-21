39fb47db13de65e623b1c9ea812a103e3fbabfc6
SadzakovicFilip <filipsadzakovic.dev@gmail.com>
2023-09-21 10:07:27 +0200

    diveded into more components, and added css

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,15 +1,28 @@
 import { Component } from "react";
+import "./card-list.styles.css";
+import SingleCard from "../card/singleCard.component";
 
-class CardList extends Component{
-    render(){
-        console.log(`card list render`)
-        const {monsters} = this.props
-        return(
-            <div>
-                {monsters.map((item,key)=><h1 key={key}>{item.name}</h1>)}
-            </div>
-        )
-    }
+class CardList extends Component {
+  render() {
+    console.log(`card list render`);
+    const { monsters } = this.props;
+    return (
+      <div className="card-list">
+        {monsters.map((item) => {
+          const { name, email, id } = item; //destructuring
+          return (
+            <SingleCard
+              name={name}
+              email={email}
+              id={id}
+              src={`https://robohash.org/${id}?set=set2&size=180x180`}
+              alt={`monster ${name}`}  
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
-export default CardList
+export default CardList;
---
+++ src/components/card/singleCard.component.jsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import "../card-list/card-list.styles.css"
+
+class SingleCard extends Component {
+  render() {
+    const { alt, src, name, email,id } = this.props;
+    return (
+      <div className="card-container" key={id}>
+        <img
+          alt={alt}
+          src={src}
+        />
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    );
+  }
+}
+
+export default SingleCard
