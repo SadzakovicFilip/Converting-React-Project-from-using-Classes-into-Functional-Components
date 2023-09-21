d0df8ed3d50b0c1aaffb2e1819007eccbe90a282
SadzakovicFilip <filipsadzakovic.dev@gmail.com>
2023-09-21 12:54:53 +0200

    Converted SIngleCard class component into functional component

--- src/components/card/singleCard.component.jsx
+++ src/components/card/singleCard.component.jsx
@@ -1,20 +1,13 @@
-import { Component } from "react";
-import "./singleCard.styles.css"
+import "./singleCard.styles.css";
 
-class SingleCard extends Component {
-  render() {
-    const { alt, src, name, email,id } = this.props;
-    return (
-      <div className="card-container" key={id}>
-        <img
-          alt={alt}
-          src={src}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+const SingleCard = ({ alt, src, name, email, id }) => {
+  return (
+    <div className="card-container" key={id}>
+      <img alt={alt} src={src} />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
-export default SingleCard
+export default SingleCard;
